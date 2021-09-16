f1d0c0cbf343eab2ce4757aa11ea9ce6d633fb2a
zabed <zabedalam777@gmail.com>
2021-09-16 12:46:35 +0200

    initialize the firebase

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyDBPAe0D8Gyu-0UBI_X1Di0beFBkVf_4BY",
+  authDomain: "onboarding-makeimpact-dev.firebaseapp.com",
+  projectId: "onboarding-makeimpact-dev",
+  storageBucket: "onboarding-makeimpact-dev.appspot.com",
+  messagingSenderId: "858534476915",
+  appId: "1:858534476915:web:c97ddac0b3459c50ff7e06"
+});
+
+const auth = app.auth();
+export { auth };
+export default app;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Onboarding from "./components/Onboarding";
+// import Onboarding from "./components/Onboarding";
+import SignUp from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello-Onboarding</h1>
-      <Onboarding></Onboarding>
+      <SignUp/>
     </div>
   );
 }
